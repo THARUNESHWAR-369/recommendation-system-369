16f48cfd361bfe5d104c5a7ec35a56f589e37370
THARUNESHWAR <s.tharuneshwar.369@gmail.com>
2021-05-26 17:39:18 +0530

    Update secret.py

--- utils/secret.py
+++ utils/secret.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class SECRET(Enum):
     API_KEY = "0935d725e4cae2bf1acd010b067db66e"
-    MOVIE_DATASET_PATH = 'main_hollywood_bollyWood_dataset.csv'#'models/main_hollywood_bollyWood_dataset.csv'
+    MOVIE_DATASET_PATH = 'movie_1916_2021_Main_dataset.csv'#'models/main_hollywood_bollyWood_dataset.csv'
     #COSINE_MODEL_PATH = "models/cosine_similarity.pkl"
     NLP_MODEL_PATH = 'models/nlp_model_randomforest.pkl'
     TRANSFORM_MODEL_PATH = 'models/tranform.pkl'
