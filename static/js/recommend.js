15257f2ca1edbf75ba3fde5a9ffbab7268c29a61
THARUNESHWAR <s.tharuneshwar.369@gmail.com>
2021-05-27 13:23:23 +0530

    Update recommend.js

--- static/js/recommend.js
+++ static/js/recommend.js
@@ -1,16 +1,21 @@
 function load_loader(e) {
 	var title = $('.search-field').val();
-	console.log(title);
-	$("#loader").fadeIn();
-	$.ajax({
-		url: '/',
-		type: 'POST',
-		data: {
-			'title': title
-		},
-		dataType: 'json',
-		success: function () {
-			$("#loader").delay(500).fadeOut();
-		},
-	});
+	if (title == '') {
+		console.log("Enter"+title);
+	}else {
+		$("#loader").fadeIn();
+		$.ajax({
+			url: '/',
+			type: 'POST',
+			data: {
+				'title': title
+			},
+			dataType: 'json',
+			success: function () {
+				$("#loader").delay(500).fadeOut();
+			},
+		});
+	}
+	
+	
 }
